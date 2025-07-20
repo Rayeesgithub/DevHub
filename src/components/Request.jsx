9d1bccb44f82e0d382ca0a68e761a782042745d7
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-07-20 16:58:16 +0530

    Update Request.jsx

--- src/components/Request.jsx
+++ src/components/Request.jsx
@@ -10,7 +10,8 @@ const Request = () => {
 
   const reviewRequest = async (status, _id) => {
     try {
-     const result = await axios.post(`${BASE_URL}/request/review/${status}/${_id}`)
+     // const result = await axios.post(`${BASE_URL}/request/review/${status}/${_id}`)
+        const result = await axios.post(BASE_URL + "/request/review" + "/" + status + "/" + _id,
         {}, {
         withCredentials: true,
       }
